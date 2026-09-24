5749cfb503d92fd225ca44fd349302a74a5803de
randyparrs <randyparra84@gmail.com>
2026-09-24 17:47:05 -0400

    Frontend: Vite project setup

---
+++ frontend/src/main.tsx
@@ -0,0 +1,24 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import './styles/litmus.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import App from './App';
+import { wagmiConfig } from './lib/wagmi';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
+);
